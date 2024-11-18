ad4fb878ae01444cf2add5368f976c3226f5b829
Gabriel M Mendes <gabrielmendes.0904@gmail.com>
2024-11-18 13:32:54 -0300

    home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,20 @@ import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { CommonModule } from '@angular/common';
 import { ExercisesComponent } from './exercises/exercises.component';
+import {HomeComponent} from './home/home.component';
 
 export type Status = 'Login' | 'Home' | 'Exercises';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, LoginComponent, ExercisesComponent],
+  imports: [RouterOutlet, CommonModule, LoginComponent, ExercisesComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'LetraLivre';
-  status: Status = 'Exercises';
+  status: Status = 'Home';
 
   // Função que muda o status da página com base num evento recebido do elemento filho
   changeStatus(targetPage: Status){
